4c1fc49eb802de177b252a2ebc6d06a329b36fc8
Umar7007 <uumarbek83@gmail.com>
2025-05-20 16:58:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let b = Math.random() * 100;
+let a = Math.round(b)
+alert(a);
+
+
+
+let c = confirm('2ga kopaytirish')
+
+
+
+if (c === true) {
+    console.log(a*2);
+    
+} else {
+    console.log('bye')
+}
